25d8121f8e9c3b1767cbfae3fc3cae70f92370ae
Andy <khanhsit11@gmail.com>
2026-07-02 23:36:22 -0700

    chore(init): initialize STG Vietnam Portal architecture

---
+++ src/constants/navigation.ts
@@ -0,0 +1,18 @@
+export const NAVIGATION = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "News",
+    href: "/news",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
---
+++ src/constants/site.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  name: "STG Vietnam",
+  company: "Strategic Sports Vietnam",
+  language: "vi",
+  email: "it@stg-vietnam.com",
+  phone: "+84 xxx xxx xxx",
+};
